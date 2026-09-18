453cc34523f5c0dfba64e30f5caa3a0e962f32ba
Amy Resnik <aresnik11@gmail.com>
2026-09-18 10:37:29 -0400

    change RadialProgress Video to component

--- packages/styleguide/src/lib/Atoms/RadialProgress/RadialProgress.stories.tsx
+++ packages/styleguide/src/lib/Atoms/RadialProgress/RadialProgress.stories.tsx
@@ -28,15 +28,8 @@ export const Children: Story = {
 };
 
 export const Animating: Story = {
-  render: () => (
-    <Box maxHeight="190px" maxWidth="190px">
-      <Video
-        autoplay
-        controls={false}
-        loop
-        videoTitle="RadialProgress animation example"
-        videoUrl="https://i.imgur.com/115O6iY.mp4"
-      />
-    </Box>
-  ),
+  args: {
+    value: [0, 100],
+    duration: 5000,
+  },
 };
