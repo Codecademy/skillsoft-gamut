e56a6e72c948e38f153617daaed10e20f1350fee
dreamwasp <cass.spain@skillsoft.com>
2026-09-22 15:47:22 -0400

    prettier

--- packages/gamut/src/ConnectedForm/ConnectedFormGroup.tsx
+++ packages/gamut/src/ConnectedForm/ConnectedFormGroup.tsx
@@ -5,12 +5,7 @@ import * as React from 'react';
 import { RegisterOptions } from 'react-hook-form';
 
 import { Anchor } from '../Anchor';
-import {
-  FormError,
-  FormGroup,
-  FormGroupLabel,
-  FormGroupProps,
-} from '../Form';
+import { FormError, FormGroup, FormGroupLabel, FormGroupProps } from '../Form';
 import { Markdown } from '../Markdown';
 import { InfoTipSubComponentProps } from '../Tip/InfoTip/type-utils';
 import { Text } from '../Typography';
